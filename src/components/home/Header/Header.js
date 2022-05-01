403cc682a580845cfef8b55b2db4658f3e6a45c5
mohiuddin2721 <mohiuddinpatwary27@gmail.com>
2022-05-01 23:38:06 +0600

    firebase and react firebase hooks installed

---
+++ src/components/home/Header/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+            <h3>This place is for navbar</h3>
+        </div>
+    );
+};
+
+export default Header;
