0c0f0ac772711a259fcdf5495ce2096c693c0a53
mohiuddin2721 <mohiuddinpatwary27@gmail.com>
2022-05-02 15:34:00 +0600

    update home page items and a button added

--- src/components/home/HomeItem/HomeItem.js
+++ src/components/home/HomeItem/HomeItem.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { Card, CardGroup } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 import './HomeItem.css';
 
 const HomeItem = props => {
-    const {titleName, price, picture, description} = props.item;
+    const { titleName, price, picture, description, quantity, supplier } = props.item;
 
     return (
-        <CardGroup className='col-md-4 col-12 border-sm rounded'>
-            <Card border='primary'>
+        <CardGroup className='col-md-4 col-12 border-sm rounded mt-5 Small shadow'>
+            <Card>
                 <Card.Img variant="top" className='w-100 rounded' src={picture} />
                 <Card.Body>
                     <Card.Title>{titleName}</Card.Title>
@@ -17,6 +18,13 @@ const HomeItem = props => {
                     <Card.Text className='p-0 m-0'>
                         {description}
                     </Card.Text>
+                    <Card.Text className='p-0 mt-2'>
+                        <span className='fs-5 fst-italic'>Quantity: {quantity}</span>
+                    </Card.Text>
+                    <Card.Text className='p-0 m-0'>
+                        Supplier: <span className='fw-normal'>{supplier}</span>
+                    </Card.Text>
+                    {/* <Link></Link> */}
                 </Card.Body>
             </Card>
         </CardGroup>
--- src/components/home/HomeItems/HomeItems.js
+++ src/components/home/HomeItems/HomeItems.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import useItems from '../../../hooks/useItems';
 import HomeItem from '../HomeItem/HomeItem';
 
@@ -8,14 +9,19 @@ const HomeItems = () => {
 
     return (
         <div>
-            <h2>Our Inventory Items</h2>
-            <div className='container'>
+            <h1 className='text-center text-primary mt-5'>Our Inventory Items</h1>
+            <div className='container row mx-auto'>
                 {
                     sixItems.map(item => <HomeItem
                         key={item.id}
                         item={item}
                     ></HomeItem>)
                 }
+            </div>
+            <div className='mt-3 text-center'>
+                <Link to='/inventories'>
+                    <button className='btn btn-primary'>See all Items</button>
+                </Link>
             </div>
         </div>
     );
