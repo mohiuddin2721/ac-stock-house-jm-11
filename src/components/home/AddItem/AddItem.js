54e4ab94f1a9ce4e28893e4c581cbd0448450aff
mohiuddin2721 <mohiuddinpatwary27@gmail.com>
2022-05-06 01:47:02 +0600

    data send from client side to server by using post method

--- src/components/home/AddItem/AddItem.js
+++ src/components/home/AddItem/AddItem.js
@@ -4,7 +4,21 @@ import { useForm } from "react-hook-form";
 
 const AddItem = () => {
     const { register, handleSubmit } = useForm();
-    const onSubmit = data => console.log(data);
+    const onSubmit = data => {
+        // console.log(data)
+        const url = `http://localhost:5000/items`;
+        fetch(url, {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        })
+        .then(res => res.json())
+        .then(result => {
+            console.log(result);
+        })
+    };
 
     return (
         <div className='mx-auto w-50 addItem-container m-4'>
@@ -16,7 +30,7 @@ const AddItem = () => {
                 <input className='mb-2' placeholder='Price' type="number" {...register("price")} />
                 <input className='mb-2' placeholder='Quantity' type="number" {...register("quantity")} />
                 <input className='mb-2' placeholder='picture' type="text" {...register("picture")} />
-                <input type="submit" />
+                <input type="submit" value="Add Service" />
             </form>
         </div>
     );
